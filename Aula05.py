90d878ec9be6649a0b5447dd085f2e3fe6ae37d1
Bielz <gabriel.rocha.silva777@gmail.com>
2024-10-23 17:35:36 -0300

    Add files via upload

---
+++ Aula05.py
@@ -0,0 +1,77 @@
+#1.
+print('Bem vindo')
+
+#2.
+print(10>3)
+
+#3.
+print('multiplicação')
+n1 = float(input('--> '))
+print(x)
+n2 = float(input('--> '))
+soma = n1 * n2
+print(soma)
+
+#4.
+print('divisão com numeros não inteiros')
+n5 = float(input('--> '))
+print('/')
+n6 = float(input('--> '))
+divisao = n5 / n6
+print(divisao)
+
+#5.
+print('subtração')
+n7 = int(input('--> '))
+print('-')
+n8 = int(input('--> '))
+sub = n7 - n8
+print(sub)
+
+#6.
+print('divisão com numeros inteiros')
+n9 = int(input('--> '))
+print('/')
+n10 = int(input('--> '))
+divisao2 = n9 / n10
+print(divisao2)
+
+#7.
+print('multiplicação com 4 numeros')
+n11 = float(input('--> '))
+print('x')
+n12 = float(input('--> '))
+print('x')
+n13 = float(input('--> '))
+print('x')
+n14 = float(input('--> '))
+soma2 = n11 * n12 * n13 * n14
+print(soma2)
+
+#8.
+numero = 100
+quadrada = 100 ** 100
+print('100')
+print('100x100=')
+print(quadrada)
+
+#9.
+print(adição)
+n15 = float(input('--> '))
+print('+')
+n16 = float(input('--> '))
+adicao = n15 + n16
+print(adicao)
+
+#10.
+nome = str(input('Digite seu nome?: '))
+moradia = str(input(nome,'onde você mora? '))
+trabalho = str(input(nome,'onde você estuda ou trabalha? '))
+cadastro  = str(input('certo, deixa eu ver se está tudo certo...'))
+print('carregando.')
+print('carregando..')
+print('carregando...')
+print('carregando.')
+print('carregando..')
+print('carregando...')
+print(nome,moradia,trabalho,cadastro)
